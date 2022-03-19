841c7874d28b50cc6fbeba897c028c1b6e7d32b5
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-19 11:45:47 +0100

    added new exercise

--- modulo-2-leccion-1.1/nuevo_nombre/index.js
+++ modulo-2-leccion-1.1/nuevo_nombre/index.js
@@ -1,1 +1,8 @@
 "use strict";
+const title = document.querySelector(".title");
+const winner = document.querySelector(".winner");
+const titleWinner = title.innerHTML + winner.innerHTML;
+
+title.innerHTML = titleWinner;
+
+console.log(titleWinner)
