79ff8d329c0453881a47fff7ec292d888bcf81f4
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-20 09:09:45 +0100

    new exercise

--- modulo-2-leccion-1.4/index.js
+++ modulo-2-leccion-1.4/index.js
@@ -1,4 +1,10 @@
 "use strict";
-const button2 = document.querySelector(".button:nth-child(2)");
-button2.classList.add("opacity");
 
+function opacitySecondButton() {
+  console.log("me he ejecutado");
+  const button2 = document.querySelector(".button:nth-child(2)");
+  button2.classList.add("opacity");
+}
+
+const button1 = document.querySelector(".button:first-child");
+button1.addEventListener("click", opacitySecondButton);
