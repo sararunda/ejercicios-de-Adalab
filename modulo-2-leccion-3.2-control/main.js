16d1e96983891d29e214ea37b5532142bd57b51c
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-22 10:28:00 +0100

    added new exercise

---
+++ modulo-2-leccion-3.2-control/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const name = "sara";
+if (name === "sara") {
+  console.log("Bienvenida," + name);
+} else {
+  console.log(
+    "lo siento pero el usuario que has introducdo no está registrado"
+  );
+}
