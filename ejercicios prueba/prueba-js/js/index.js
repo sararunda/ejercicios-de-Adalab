0b4db3f5bc8fa56dfba28cb45f297abb94b4e045
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-25 17:42:25 +0100

    add event collapse

--- ejercicios prueba/prueba-js/js/index.js
+++ ejercicios prueba/prueba-js/js/index.js
@@ -1,24 +1,49 @@
 "use strict";
+// //
+// //EJERCICIO DE EVENTO, FUNCIONES Y CONDICIONALES
+// const nameInput = document.querySelector(".js-name");
+// const lastnameInput = document.querySelector(".js-lastname");
+// const buttonInput = document.querySelector(".js-button");
+// const textAlert = document.querySelector(".js-alert");
+// nameInput.value = "Sara";
+// lastnameInput.value = "Fernández Ramos";
+// //
+// //está función es para que al hacer click devuelva un resultado
+// function handleClick(event) {
+//   const name = nameInput.value;
+//   const lastname = lastnameInput.value;
+//   if (name === "Sara" && lastname === "Fernández Ramos") {
+//     textAlert.innerHTML = `Bienvenida Adalaber ${name} ${lastname}`;
+//   } else {
+//     textAlert.innerHTML = `No puedes entrar`;
+//   }
+// }
+// //escucho el click del botón seguido de la función que quiero que haga
+// buttonInput.addEventListener("click", handleClick);
+// //
+// //
 //
 //
-const nameInput = document.querySelector(".js-name");
-const lastnameInput = document.querySelector(".js-lastname");
-const buttonInput = document.querySelector(".js-button");
-const textAlert = document.querySelector(".js-alert");
-nameInput.value = "sara";
-lastnameInput.value = "fernandez";
-//
-//está función es para que al hacer click devuelva un resultado
-function handleClick(event) {
-  const name = nameInput.value;
-  const lastname = lastnameInput.value;
-  if (name === "sara") {
-    textAlert.innerHTML = `Bienvenida Adalaber ${name} ${lastname}`;
+//variables
+const list = document.querySelector(".js-list");
+list.innerHTML += `<li>Mayor de edad</li>`;
+list.innerHTML += `<li>Tener mascota</li>`;
+list.innerHTML += `<li>Documentacion</li>`;
+const newButton = document.querySelector(".js-new-button");
+newButton.innerHTML = `<input
+type="button"
+name="aceptar"
+id="aceptar"
+value="aceptar"
+class="js-button"
+/>`;
+//funcion
+function collapsed() {
+  if (list.classList.contains("hidden")) {
+    list.classList.remove("hidden");
   } else {
-    textAlert.innerHTML = `No puedes entrar`;
+    list.classList.add("hidden");
   }
 }
-//escucho el click del botón seguido de la función que quiero que haga
-buttonInput.addEventListener("click", handleClick);
-//
-//
+//evento
+newButton.addEventListener("click", collapsed);
