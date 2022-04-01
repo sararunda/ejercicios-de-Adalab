b42646e0d447030824f2d04f95d3ff0c9253076e
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-01 08:19:41 +0200

    add arrayss exercise

---
+++ modulo-2-arrays/js/index.js
@@ -0,0 +1,44 @@
+'use strict';
+const adalabers = [
+  {
+    nombre: 'maria',
+    edad: 29,
+    profesión: 'diseñadora',
+  },
+  {
+    nombre: 'lucia',
+    edad: 31,
+    profesión: 'ingeniera',
+  },
+  {
+    nombre: 'susana',
+    edad: 34,
+    profesión: 'periodista',
+  },
+  {
+    nombre: 'rocio',
+    edad: 25,
+    profesión: 'actriz',
+  },
+  {
+    nombre: 'inmaculada',
+    edad: 21,
+    profesión: 'diseñadora',
+  },
+];
+//numero de adalabers en el listado
+function countAdalabers(adalabersArray) {
+  const count = adalabersArray.length;
+  return count;
+}
+const adalabersNum = countAdalabers(adalabers);
+console.log(adalabersNum);
+
+//media de edad del listado
+function averageAge(adalabersArray) {
+    const media = 
+}
+//nombre de las mas joven
+function theYoungest() {}
+//numero de diseñadoras
+function countDesigners() {}
