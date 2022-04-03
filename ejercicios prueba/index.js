9851e32690e5f488dbfdf56a0ebb1f5e3f675d3d
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-03 10:03:48 +0200

    fix some errors

--- ejercicios prueba/index.js
+++ ejercicios prueba/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const x = 20;
 const y = 10;
 // const result = x + y;
@@ -8,39 +8,39 @@ const a = 30;
 // console.log("media", result);
 //
 // const name = "sara";
-const surname = "fernandez";
+const surname = 'fernandez';
 const year = 20;
 // console.log("Mi nombre es", name, "y mi apellido es", surname);
 const content = `Mi nombre es ${name}, mi apellido es ${surname} y tengo ${year} años`;
 // console.log(content);
 // console.log(name === "sara" || surname === "fernandez");
-if (name === "sara" && surname === "fernandez") {
+if (name === 'sara' && surname === 'fernandez') {
   //   console.log("ok sara");
-} else if (name == "pablo" && surname === "marquez") {
+} else if (name == 'pablo' && surname === 'marquez') {
   //   console.log("ok pablo");
 } else {
   //   console.log("no");
 }
-const nose = name === "sara" && surname === "fernandez" ? "ok sara" : "no";
+const nose = name === 'sara' && surname === 'fernandez' ? 'ok sara' : 'no';
 // console.log(nose);
-if (name === "sara" && surname === "fernandez" && year > "20") {
+if (name === 'sara' && surname === 'fernandez' && year > '20') {
   // console.log(content);
 }
-const inputName = document.querySelector(".js_name");
-const inputLastname = document.querySelector(".js_lastName");
-const button = document.querySelector(".js_button");
-const result = document.querySelector(".js_result");
-const title = document.querySelector(".js_title");
-const lastPreview = document.querySelector(".js_last");
-button.addEventListener("click", () => {
-  // valor que escribe la usuaria
+const inputName = document.querySelector('.js_name');
+const inputLastname = document.querySelector('.js_lastName');
+const button = document.querySelector('.js_button');
+const result = document.querySelector('.js_result');
+const title = document.querySelector('.js_title');
+const lastPreview = document.querySelector('.js_last');
+button.addEventListener('click', () => {
+  // valor que escribe la usuaria//
   const name = inputName.value;
   const lastname = inputLastname.value;
   result.innerHTML = name + lastname;
 });
-inputName.addEventListener("keyup", () => {
+inputName.addEventListener('keyup', () => {
   title.innerHTML = inputLastname.value;
 });
-inputLastname.addEventListener("keyup", () => {
+inputLastname.addEventListener('keyup', () => {
   lastPreview.innerHTML = inputLastname.value;
 });
