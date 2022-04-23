e1d0456d190f4171bc677b1af1f6eabee21bfc5d
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-23 22:02:48 +0200

    add render list preview

---
+++ modulo-3-react/repaso-intermedio-react/src/components/App.js
@@ -0,0 +1,126 @@
+import '../styles/App.scss';
+import { useState } from 'react';
+
+function App() {
+  //state variables
+  const [inputForm, setInputForm] = useState({
+    name: '',
+    lastname: '',
+    phone: '',
+    email: '',
+  });
+  const [contactList, setContactList] = useState([]);
+
+  //functions
+  const renderList = () => {
+    console.log('contact list', contactList);
+    return contactList.map((data, index) => (
+      <li key={index} className="contact__item">
+        <p className="contact__name">
+          <label className="contact__label">Nombre:</label>
+          {data.name}
+          {data.lastname}
+        </p>
+        <p className="contact__phone">
+          <label className="contact__label">Teléfono:</label>
+          <a href="tel:603256679" title="Pulsa aquí para llamar a Jane">
+            {data.phone}
+          </a>
+        </p>
+        <p className="contact__mail">
+          <label className="contact__label">Email:</label>
+          <a
+            href="mailto:janenorton@adalab.es"
+            title="Pulsa aquí para escribir a Jane"
+          >
+            {data.email}
+          </a>
+        </p>
+      </li>
+    ));
+  };
+
+  //funciones manejadoras
+  const handleFormChange = (ev) => {
+    const inputName = ev.target.name;
+    const inputValue = ev.target.value;
+
+    setInputForm({
+      ...inputForm,
+      [inputName]: inputValue,
+    });
+  };
+
+  const handleClickButton = (ev) => {
+    ev.preventDefault();
+    setContactList([...contactList, inputForm]);
+    console.log(contactList);
+  };
+  return (
+    <div className="page">
+      {/* header */}
+      <header className="header">
+        <h1 className="header__title">Mi agenda de contactos</h1>
+        <form>
+          <input
+            className="header__search"
+            autoComplete="off"
+            type="search"
+            name="search"
+            placeholder="Filtrar contactos por nombre"
+          />
+        </form>
+      </header>
+
+      <main>
+        {/* contact list */}
+        <ul className="contact__list">{renderList()}</ul>
+
+        {/* new contact */}
+        <form className="new-contact__form" onChange={handleFormChange}>
+          <h2 className="new-contact__title">Añade un nuevo contacto</h2>
+          <input
+            className="new-contact__input"
+            type="text"
+            name="name"
+            id="name"
+            value={inputForm.name}
+            placeholder="Nombre"
+          />
+          <input
+            className="new-contact__input"
+            type="text"
+            name="lastname"
+            id="lastname"
+            value={inputForm.lastname}
+            placeholder="Apellidos"
+          />
+          <input
+            className="new-contact__input"
+            type="phone"
+            name="phone"
+            value={inputForm.phone}
+            id="phone"
+            placeholder="Teléfono"
+          />
+          <input
+            className="new-contact__input"
+            type="email"
+            name="email"
+            id="email"
+            value={inputForm.email}
+            placeholder="Email"
+          />
+          <input
+            onClick={handleClickButton}
+            className="new-contact__btn"
+            type="submit"
+            value="Añadir"
+          />
+        </form>
+      </main>
+    </div>
+  );
+}
+
+export default App;
